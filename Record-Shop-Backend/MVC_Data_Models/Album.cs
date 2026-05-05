ebd18abd76d0737b2121a62aa407ce4918fa3aa7
Kati Stark <kati.stark95@gmail.com>
2026-05-05 10:09:40 +0100

    Removed template Weatherforecast, add MVC_Data_Models

---
+++ Record-Shop-Backend/MVC_Data_Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Record_Shop_Backend.MVC_Data_Models
+{
+    public class Album
+    {
+        [JsonPropertyName("id")]
+        public int AlbumId { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("released")]
+        public string ReleaseYear { get; set; }
+        [JsonPropertyName("genre")]
+        public string Genre { get; set; }
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+        [JsonPropertyName("stock")]
+        public int Stock { get; set; }
+    }
+}
