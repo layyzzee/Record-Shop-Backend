b76b733bd3fbb42a5140fd9dc39c1c60b2868a41
Kati Stark <kati.stark95@gmail.com>
2026-05-05 10:18:05 +0100

    Add skeleton AlbumController

---
+++ Record-Shop-Backend/MVC_Controllers/AlbumController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Record_Shop_Backend.MVC_Services;
+
+namespace Record_Shop_Backend.MVC_Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IAlbumService _albumService;
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+    }
+}
