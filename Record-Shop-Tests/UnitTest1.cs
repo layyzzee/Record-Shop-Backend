a0fdf2173efd8e2e015cb146d3dd8eeea30a0314
Kati Stark <kati.stark95@gmail.com>
2026-05-05 10:20:47 +0100

    Add Testing project to utilise TDD for each new method

---
+++ Record-Shop-Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Record_Shop_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
