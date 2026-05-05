bf737952b12d9e73540add5adc23bb01ebcc2136
Kati Stark <kati.stark95@gmail.com>
2026-05-05 10:15:43 +0100

    Add skeleton AlbumService

---
+++ Record-Shop-Backend/MVC_Services/AlbumService.cs
@@ -0,0 +1,17 @@
+﻿using Record_Shop_Backend.MVC_Repositories;
+
+namespace Record_Shop_Backend.MVC_Services
+{
+    public interface IAlbumService
+    {
+
+    }
+    public class AlbumService : IAlbumService
+    {
+        private readonly IAlbumRepository _albumRepository;
+        public AlbumService(IAlbumRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+        }
+    }
+}
